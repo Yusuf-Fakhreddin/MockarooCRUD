12118473f5b9b69acefb204e5a31f973692b5491
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-02-09 13:20:36 +0200

    Initial commit

---
+++ MockarooCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MockarooCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
